4b04228965901a7c70e369d7edbdc1affbc2dfef
mehedimahid <mehedimahid19@gmail.com>
2021-03-01 19:10:07 +0600

    Error handling

--- Chepter Sixteen/src/index.js
+++ Chepter Sixteen/src/index.js
@@ -8,11 +8,116 @@ import "./style.scss";
 //     throw new Error('N is getter then 5')
 // }
 
-let rang =56
+// let rang =56
 
-if(rang > 50){
-    throw new RangeError('the number is too long')
+// if(rang > 50){
+//     throw new RangeError('the number is too long')
+// }
+
+
+                        //171 Error Handling with If Else in Javascript
+
+// function changToInt(v){
+//     let result = Number.parseInt(v)
+//     if(!result){
+//         return 'Please provide a value which is able to convert ib integer'
+        
+//     }
+//     return result
+// }
+
+// // let result = changToInt('adffg85.66')
+// let result = changToInt('88.66')
+
+// console.log(result)
+
+
+                            //172 Error Handling with Try Catch in Javascript |
+
+// function makeWords(text){
+//     try{
+//         let str = text.trim()
+//         let words = str.split('  ')
+//         return words
+//     }catch(e){
+//         // console.log(e.message)
+//         console.log('Please provide a valid text')
+//     }
+//     // let str = text.trim()
+//     //     let words = str.split('  ')
+//     //     return words
+// }
+
+// // let words = makeWords(85)
+// let words = makeWords('        I am MH Mahid        ')
+// console.log(words)
+
+
+                            //173 Throwing an Error in Javascript 
+
+// try{
+//     console.log('i am line 1')
+//     throw new Error('i am your Error')
+//     console.log('i am line 2')
+//     console.log('i am line 3')
+//     console.log('i am line 4')
+// }catch(e){
+//     console.log(e.message)
+// }
+
+
+                    //174 Finally Block in Javascript 
+
+// try{
+//      console.log('i am line 1')
+//     throw new Error('i am your Error')
+//     console.log('i am line 2')
+//     console.log('i am line 3')
+//     console.log('i am line 4')
+// }catch(e){
+//     console.log(e.message)
+// }finally{
+//     console.log('i am finaly block')
+// }
+
+
+                    //175 Optional Catch Binding in Javascript
+// try{
+//     console.log('i am line 1')
+//    throw new Error('i am your Error')
+//    console.log('i am line 2')
+//    console.log('i am line 3')
+//    console.log('i am line 4')
+// }catch{
+//    console.log('This is a Custom Error message')
+// }finally{
+//    console.log('i am finaly block')
+// }
+
+
+                //176 Custom Error in Javascript
+
+class CustomError extends Error{
+    constructor(msg){
+        super(msg)
+        if (Error.captureStackTrace){
+            Error.captureStackTrace(this, CustomError)
+        }
+    }
 }
+
+ try{
+        console.log('i am line 1')
+       throw new CustomError('i am your Error')
+       console.log('i am line 2')
+       console.log('i am line 3')
+       console.log('i am line 4')
+    }catch(e){
+        console.dir(e)
+        console.log('This is a Custom Error message')
+    }finally{
+       console.log('i am finaly block')
+    }
 
 
 
