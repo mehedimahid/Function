8ec6d1b71205da755f847da915b79bcbe9ef1316
mehedimahid <mehedimahid19@gmail.com>
2021-03-20 15:37:49 +0600

    Callback,Promise

--- Chepter Seventeen/src/lessons/lesson1.js
+++ Chepter Seventeen/src/lessons/lesson1.js
@@ -71,44 +71,173 @@
 // xhr.send()
 
 
-function getRequest(url, callback) {
-    const xhr = new XMLHttpRequest()
-    xhr.open('get', url)
-    xhr.onreadystatechange = function(e) {
-        if (xhr.readyState === 4){
-            if(xhr.status === 200){      
-                let response = JSON.parse(xhr.response)
-                callback(null, response)
-            }else{
-                callback(xhr.status, null)
-            }
-        }
-    }
+// function getRequest(url, callback) {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open('get', url)
+//     xhr.onreadystatechange = function(e) {
+//         if (xhr.readyState === 4){
+//             if(xhr.status === 200){      
+//                 let response = JSON.parse(xhr.response)
+//                 callback(null, response)
+//             }else{
+//                 callback(xhr.status, null)
+//             }
+//         }
+//     }
     
-    xhr.send()
-}
+//     xhr.send()
+// }
 
 
-getRequest('http://jsonplaceholder.typicode.com/users', (err, res) =>{
-    if(err){
-        console.log(err)
-    }else{
-        // console.log(res)
-        res.forEach(r => console.log(r.name))
-    }
-})
+// getRequest('http://jsonplaceholder.typicode.com/users', (err, res) =>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         // console.log(res)
+//         res.forEach(r => console.log(r.name))
+//     }
+// })
 
 
-getRequest('http://jsonplaceholder.typicode.com/posts', (err, res) =>{
-    if(err){
-        console.log(err)
-    }else{
-        // console.log(res)
-        res.forEach(r => console.log(r.title))      //r = singular post obj portek p. obj. ar vitor title ase
+// getRequest('http://jsonplaceholder.typicode.com/posts', (err, res) =>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         // console.log(res)
+//         res.forEach(r => console.log(r.title))      //r = singular post obj portek p. obj. ar vitor title ase
             
         
-    }
-})
+//     }
+// })
+
+
+                            //186 Callback Not Always Asynchronous in Javascript
+
+
+// let arr =[1, 2, 3, 4, 5]
+
+// let sqrArr = arr.map(v => v*v)
+// console.log(sqrArr)
+
+
+// function asynMap(arr, cb) {
+//     return arr.map(v =>{
+//         // setTimeout(cb.bind(null, v), 0)
+//         setTimeout(() => cb(v), 0)
+//     })
+// }
+
+// let qbArr = asynMap(arr, v =>{
+//     console.log(v)
+// })
+// console.log(qbArr)
+
+
+
+                            //187 Why Not Callback in Javascript 
+                            //real code noy don't run this
+
+// function getRequest(url, callback) {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open('get', url)
+//     xhr.onreadystatechange = function(e) {
+//         if (xhr.readyState === 4){
+//             if(xhr.status === 200){      
+//                 let response = JSON.parse(xhr.response)
+//                 callback(null, response)
+//             }else{
+//                 callback(xhr.status, null)
+//             }
+//         }
+//     }
+    
+//     xhr.send()
+// }
+
+// const BASE_URl = 'http://jsonplaceholder.typicode.com'
+
+
+// getRequest (`${BASE_URl}/posts/1`, (err, res) =>{
+//     if(err){
+//         throw new Error('Error Occurred')
+//     }
+
+//     let {userId} = res
+
+//     getRequest(`${BASE_URl}/users/${userId}`, (err, res) =>{
+//         if(err){
+
+//             throw new Error('Error Occurred')
+//         }
+
+//         getRequest(`${BASE_URl}/posts/comments/${res.id}`,(err, res) =>{
+
+//             if(err){
+
+//             throw new Error('Error Occurred')
+//             }
+
+//             console.log(res)
+//         })
+//     })
+// })
+
+
+                        //188 Introduction to Promise in Javascript
+
+
+// let P1 = new Promise((resolve, reject) => {
+
+//     setTimeout(resolve, 5000, 'One')  
+// })
+
+// let P2 = new Promise((resolve,reject) =>{
+//     setTimeout(resolve, 3000, 'Two')
+// })
+
+// // console.log(P1)
+// // console.log(P2)
+
+// P1.then((v) =>{
+//     console.log(v)
+// })
+
+// P2.then((v) =>{
+//     console.log(v)
+// })
+
+
+
+function getIphone(isPassed) {
+    return new Promise((resolve, reject) => {
+        setTimeout (() =>{
+            if(isPassed){
+                resolve('I have got an I Phone')
+            } else{
+                reject(new Error('you have failed'))
+            }
+        },2000)
+    })
+}
+
+// console.log(getIphone(true))
+// console.log(getIphone(false))
+
+getIphone(false)
+    .then((v) =>{
+        console.log(v)
+    })
+    .catch((e) => {
+        // console.log(e)
+        console.log(e.message)
+    })
+
+
+
+
+
+
+
 
 
 
