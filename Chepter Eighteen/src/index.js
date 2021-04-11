9571e5ba5bb57d908cd6a0c65d6e048e7be4b1ed
mehedimahid <mehedimahid19@gmail.com>
2021-04-12 00:47:21 +0600

    DOM

---
+++ Chepter Eighteen/src/index.js
@@ -0,0 +1,8 @@
+// import "./style.css";
+
+import "./style.scss";
+
+import './lessons/lesson1.js'
+// import './lessons/lesson2.js'
+// import './lessons/lesson3.js'
+// import './lessons/lesson4.js'
---
+++ Chepter Eighteen/src/lessons/lesson1.js
@@ -0,0 +1,47 @@
+                    //198 Selectors Part One in Javascript 
+
+
+
+// let H1 = document.getElementById('title')
+// console.log(title)
+
+// let Pragraphs = document.getElementsByClassName('Lead')
+// console.log(Pragraphs)
+
+// let List = document.getElementsByTagName('li')
+// console.log(List)
+
+// let listNameOne = document.getElementsByName('list-name-one')
+// console.log(listNameOne)
+
+                    //199 Selectors Part Two in Javascript
+// let h1 = document.querySelector('#title')
+// console.log(h1)
+
+// let pragraph = document.querySelector('.Lead')
+// console.log(pragraph)
+
+// let pragraphs = document.querySelectorAll('.Lead')
+// console.log(pragraphs)
+
+// let lists = document.querySelectorAll('li')
+// console.log(lists)
+
+// let listNameOne = document.querySelector('[name^="list-name-one"]')
+// console.log(listNameOne)
+
+
+                //200 GetElemetns Method vs Query Selector in Javascript
+
+
+// let list1 = document.getElementsByTagName('li')
+
+// let list2 = document.querySelectorAll('li')
+
+// console.log(list1.constructor.name)
+// console.log(list2.constructor.name)
+
+
+let ul = document.querySelector('ul')
+console.log(ul.children)
+console.log(ul.childNodes)
